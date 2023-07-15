12967061ec22cd73accecc293a7f8c66379b5ca7
Rendi Okriza <rendiokrizaputra@gmail.com>
2023-07-15 23:59:42 +0700

    fix: button and button group

--- src/colors/base-colors.ts
+++ src/colors/base-colors.ts
@@ -1,19 +1,19 @@
-import { BaseTheme } from "../types/theme.types";
+import { BaseTheme } from '../types/theme.types'
 
 export const baseColors: BaseTheme = {
-  neutral: "105 113 119",
-  primary: "96 165 250",
-  secondary: "244 114 182",
-  success: "74 222 128",
-  error: "248 113 113",
-  warning: "250 204 21",
-};
+  neutral: '105 113 119',
+  primary: '96 165 250',
+  secondary: '244 114 182',
+  success: '74 222 128',
+  error: '248 113 113',
+  warning: '250 204 21'
+}
 
 export const baseColorsDark: BaseTheme = {
-  neutral: "105 113 119",
-  primary: "96 165 250",
-  secondary: "244 114 182",
-  success: "74 222 128",
-  error: "248 113 113",
-  warning: "250 204 21",
-};
+  neutral: '105 113 119',
+  primary: '96 165 250',
+  secondary: '244 114 182',
+  success: '74 222 128',
+  error: '248 113 113',
+  warning: '250 204 21'
+}
--- src/docs/components/content/component/ButtonGroup.vue
+++ src/docs/components/content/component/ButtonGroup.vue
@@ -16,20 +16,18 @@ defineProps({
 </script>
 
 <template>
-  <div class="">
-    <div class="btn-group btn-group-scrollable" :class="styleClass">
-      <template v-if="btn">
-        <button class="btn">Primary</button>
-        <button class="btn" :class="btnStyle">Primary</button>
-        <button class="btn">Primary</button>
-        <button class="btn">Primary</button>
-      </template>
-      <template v-else>
-        <input type="radio" name="options" data-content="1" class="btn" />
-        <input type="radio" name="options" data-content="2" class="btn" />
-        <input type="radio" name="options" data-content="3" class="btn" />
-        <input type="radio" name="options" data-content="4" class="btn" />
-      </template>
-    </div>
+  <div class="btn-group btn-group-scrollable not-prose" :class="styleClass">
+    <template v-if="btn">
+      <button class="btn">Primary</button>
+      <button class="btn" :class="btnStyle">Primary</button>
+      <button class="btn">Primary</button>
+      <button class="btn">Primary</button>
+    </template>
+    <template v-else>
+      <input type="radio" name="options" data-content="1" class="btn" />
+      <input type="radio" name="options" data-content="2" class="btn" />
+      <input type="radio" name="options" data-content="3" class="btn" />
+      <input type="radio" name="options" data-content="4" class="btn" />
+    </template>
   </div>
 </template>
