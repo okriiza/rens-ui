ce27338c3424cd4524d663c7ad69b6ad00feb1a1
Rendi Okriza <rendiokrizaputra@gmail.com>
2023-07-15 18:41:19 +0700

    feat: button-group

---
+++ src/docs/components/content/component/ButtonGroup.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  btnStyle: {
+    type: String,
+    default: ''
+  },
+  styleClass: {
+    type: String,
+    default: ''
+  },
+  btn: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <div class="">
+    <div class="btn-group btn-group-scrollable" :class="styleClass">
+      <template v-if="btn">
+        <button class="btn">Primary</button>
+        <button class="btn" :class="btnStyle">Primary</button>
+        <button class="btn">Primary</button>
+        <button class="btn">Primary</button>
+      </template>
+      <template v-else>
+        <input type="radio" name="options" data-content="1" class="btn" />
+        <input type="radio" name="options" data-content="2" class="btn" />
+        <input type="radio" name="options" data-content="3" class="btn" />
+        <input type="radio" name="options" data-content="4" class="btn" />
+      </template>
+    </div>
+  </div>
+</template>
