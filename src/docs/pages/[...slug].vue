86393e51bce237548969d53c33cf6c337f9c47c4
Rendi Okriza <rendiokrizaputra@gmail.com>
2023-07-16 19:13:47 +0700

    fix: scroll sticky aside and Toc

--- src/docs/pages/[...slug].vue
+++ src/docs/pages/[...slug].vue
@@ -12,7 +12,7 @@ definePageMeta({
     <DocsNav />
     <div class="container mx-auto mt-6 flex flex-row space-x-8 px-28 pt-20">
       <div
-        class="sticky top-16 h-[calc(100vh-theme(spacing.16))] w-1/6 overflow-y-auto"
+        class="sticky top-24 h-[calc(100vh-theme(spacing.16))] w-1/6 overflow-y-auto"
       >
         <ul>
           <li v-for="link of navigation" :key="link._path">
@@ -43,7 +43,7 @@ definePageMeta({
       </div>
       <div class="w-4/6 px-6">
         <div
-          class="prose max-w-full dark:prose-invert prose-a:no-underline prose-thead:bg-gray-3 prose-th:px-3 prose-th:py-2 prose-td:px-3 prose-td:py-2 prose-thead:rounded-t-lg"
+          class="prose max-w-full dark:prose-invert prose-a:no-underline prose-thead:bg-gray-3 prose-th:px-3 prose-th:py-2 prose-td:px-3 prose-td:py-2 prose-thead:rounded-t-lg prose-headings:scroll-mt-24"
         >
           <ContentDoc>
             <template #not-found>
@@ -76,21 +76,23 @@ definePageMeta({
         </div>
       </div>
       <div class="w-1/6">
-        <h2 class="text-lg font-bold">Table Of Contents</h2>
-        <ul v-if="toc && toc.links" class="mt-3">
-          <li
-            v-for="link in toc.links"
-            :key="link.text"
-            class="ml-0.5 space-y-1 border-l border-gray-3 text-sm"
-          >
-            <a
-              :href="`#${link.id}`"
-              class="-ml-px block border-l border-transparent py-1 pl-4 text-sm text-slate-8 hover:border-blue-4 hover:text-blue-4"
+        <div class="sticky top-24">
+          <h2 class="text-lg font-bold">Table Of Contents</h2>
+          <ul v-if="toc && toc.links" class="mt-3">
+            <li
+              v-for="link in toc.links"
+              :key="link.text"
+              class="ml-0.5 space-y-1 border-l border-gray-3 text-sm"
             >
-              {{ link.text }}
-            </a>
-          </li>
-        </ul>
+              <a
+                :href="`#${link.id}`"
+                class="-ml-px block border-l border-transparent py-1 pl-4 text-sm text-slate-8 hover:border-blue-4 hover:text-blue-4"
+              >
+                {{ link.text }}
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
     <Footer />
